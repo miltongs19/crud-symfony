a07f6cc18cfa3bed6b4ce880b74d6a636d74b273
Milton Gomes de Sousa <miltongs19@gmail.com>
2018-09-03 23:10:07 -0300

    Criacao das migrations e metodo save para o Article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,5 +19,23 @@ class ArticleController extends AbstractController
         return $this->render('articles/index.html.twig',[
             'articles' => $articles
         ]);
+    }
+
+    /**
+     * @Route("/article/save")
+     */
+    public function save()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        // Teste com alto acoplamento, o teste com DI será feito posteriormente
+        $article = new Article();
+        $article->setTitle('Title 1');
+        $article->setBody('Body 1');
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return Response('Artigo salvo com id: ' . $article->getId());
     }
 }
